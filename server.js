fb341977b4185d5a8f05538c05994c11fd51fc49
sifracapitalgroup <sifra.capitalgroup@gmail.com>
2026-05-18 20:46:11 -0400

    updated opening lines with few additions

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ function buildOpenerSpeechContext(lead) {
 /** Mandatory first lines (session + response.create). Server fills name/location only. */
 function buildFixedOutboundOpenerScript(ctx) {
   const hi = `Hi ${ctx.sellerName}...?`;
-  const ask = `This is Daniel - Would you sell your property on ${ctx.locationClause}?`;
+  const ask = `This is Daniel... Would you be open to selling your property on ${ctx.locationClause}?`;
   return { hi, ask };
 }
 
